1d2531dc639de091d382be2af729df13ae4f62d1
Vsevolod Lykhobaba <lihobabaseva2006@gmail.com>
2024-08-29 20:37:11 +0200

    Final commit

--- main.py
+++ main.py
@@ -33,8 +33,8 @@ user_data = {}
 @bot.message_handler(commands=['start'])
 def start(message):
     user_data[message.chat.id] = {}
-    bot.reply_to(message, 'Доброго дня, давайте зберемо дані про вас.')
-    msg = bot.send_message(message.chat.id, "Введіть ваш ПІБ. Приклад:\nБабай Генадій Генадійович")
+    bot.reply_to(message, "Good afternoon, let's collect information about you.")
+    msg = bot.send_message(message.chat.id, "Enter your PIB. Example:\nBabay Gennady Gennadiyovich")
     bot.register_next_step_handler(msg, process_name)
 
 def process_name(message):
@@ -42,10 +42,10 @@ def process_name(message):
     name = message.text
     if validate_name(name):
         user_data[chat_id]['name'] = name
-        msg = bot.send_message(chat_id, 'Введіть ваш номер телефону. Приклад:\n+380000000000')
+        msg = bot.send_message(chat_id, 'Enter your phone number. Butt:\n+380000000000')
         bot.register_next_step_handler(msg, process_phone)
     else:
-        msg = bot.send_message(chat_id, 'В вашому імені знайдено не кириличні символи введіть заново.')
+        msg = bot.send_message(chat_id, 'Non-Cyrillic characters were found in your name, please re-enter.')
         bot.register_next_step_handler(msg, process_name)
 
 def process_phone(message):
@@ -53,10 +53,10 @@ def process_phone(message):
     phone = message.text
     if validate_phone(phone):
         user_data[chat_id]['phone'] = phone
-        msg = bot.send_message(chat_id, 'Введіть ваш ІНН. Приклад:\n333033024')
+        msg = bot.send_message(chat_id, 'Enter your INN. Butt:\n333033024')
         bot.register_next_step_handler(msg, process_inn)
     else:
-        msg = bot.send_message(chat_id, 'Некоректний номер телефону введіть ще раз:')
+        msg = bot.send_message(chat_id, 'Enter the incorrect phone number again:')
         bot.register_next_step_handler(msg, process_phone)
 
 def process_inn(message):
@@ -64,10 +64,10 @@ def process_inn(message):
     inn = message.text
     if validate_inn(inn):
         user_data[chat_id]['inn'] = inn
-        msg = bot.send_message(chat_id, 'Введіть вашу дату народження (ДД.ММ.ГГГГ). Приклад:\n26.09.2006')
+        msg = bot.send_message(chat_id, 'Enter your date of birth (DD.MM.YYYY). Butt:\n26.09.2006')
         bot.register_next_step_handler(msg, process_dob)
     else:
-        msg = bot.send_message(chat_id, 'Некоректний ІНН введіть ще раз:')
+        msg = bot.send_message(chat_id, 'Enter the incorrect INN again:')
         bot.register_next_step_handler(msg, process_inn)
 
 def process_dob(message):
@@ -76,11 +76,11 @@ def process_dob(message):
     if validate_dob(dob):
         user_data[chat_id]['dob'] = dob
         markup = types.InlineKeyboardMarkup()
-        markup.add(types.InlineKeyboardButton("Так", callback_data="vpo_yes"),
-                   types.InlineKeyboardButton("Ні", callback_data="vpo_no"))
-        bot.send_message(chat_id, 'Ви ВПО? (Так/Ні)', reply_markup=markup)
+        markup.add(types.InlineKeyboardButton("Yes", callback_data="vpo_yes"),
+                   types.InlineKeyboardButton("No", callback_data="vpo_no"))
+        bot.send_message(chat_id, 'Are you a VPO? (Yes/No)', reply_markup=markup)
     else:
-        msg = bot.send_message(chat_id, 'Некоректна дата народження введіть ще раз (ДД.ММ.ГГГГ):')
+        msg = bot.send_message(chat_id, 'The date of the people is incorrect, please enter it again (DD.MM.YYYY):')
         bot.register_next_step_handler(msg, process_dob)
 
 @bot.callback_query_handler(func=lambda call: call.data in ["vpo_yes", "vpo_no"])
@@ -91,7 +91,7 @@ def process_vpo(call):
         return
     
     user_data[chat_id]['vpo'] = 'Да' if call.data == 'vpo_yes' else 'Ні'
-    msg = bot.send_message(chat_id, 'Вкажіть локацію в якій знаходитесь. Приклад:\nЛюботин, вул.Залупіна, 6')
+    msg = bot.send_message(chat_id, 'Indicate the location where you are located. Stock:\nLyubotin, Zalupina st., 6')
     if call.data == 'vpo_no':
         bot.register_next_step_handler(msg, process_location_and_vulnerability)
     else:
@@ -107,10 +107,10 @@ def process_location_and_vulnerability(message):
     
     markup = types.InlineKeyboardMarkup()
     markup.add(types.InlineKeyboardButton("65+", callback_data="vulnerability_65_plus"),
-               types.InlineKeyboardButton("Багатодітна сім'я", callback_data="vulnerability_large_family"),
-               types.InlineKeyboardButton("Наявність інвалідності", callback_data="vulnerability_disability"),
-               types.InlineKeyboardButton("Інше", callback_data="vulnerability_other"))
-    bot.send_message(chat_id, 'До якої вразливої категорії ви відноситесь?', reply_markup=markup)
+               types.InlineKeyboardButton("Rich family", callback_data="vulnerability_large_family"),
+               types.InlineKeyboardButton("Detection of disability", callback_data="vulnerability_disability"),
+               types.InlineKeyboardButton("Other", callback_data="vulnerability_other"))
+    bot.send_message(chat_id, 'What category do you fall into?', reply_markup=markup)
 
 @bot.callback_query_handler(func=lambda call: call.data.startswith("vulnerability_"))
 def process_vulnerability(call):
@@ -120,13 +120,13 @@ def process_vulnerability(call):
         return
 
     if call.data == "vulnerability_other":
-        msg = bot.send_message(chat_id, 'Будь ласка вкажіть ваш варіант:')
+        msg = bot.send_message(chat_id, 'Please indicate your option:')
         bot.register_next_step_handler(msg, process_vulnerability_other)
     else:
         vulnerability_options = {
             "vulnerability_65_plus": "65+",
-            "vulnerability_large_family": "Багатодітна сім'я",
-            "vulnerability_disability": "Наявність інвалідності"
+            "vulnerability_large_family": "Rich family",
+            "vulnerability_disability": "Detection of disability"
         }
         user_data[chat_id]['vulnerability'] = vulnerability_options[call.data]
         ask_assistance(chat_id)
@@ -152,13 +152,13 @@ def process_location(message):
 
 def ask_assistance(chat_id):
     markup = types.InlineKeyboardMarkup()
-    markup.add(types.InlineKeyboardButton("Харчування", callback_data="assistance_food"),
-               types.InlineKeyboardButton("Гігієнічний набір", callback_data="assistance_hygiene_set"),
-               types.InlineKeyboardButton("Ліки", callback_data="assistance_meds"),
-               types.InlineKeyboardButton("Засоби гігієни(Памперси, прокладки, пелюшки)", callback_data="assistance_hygiene_items"),
-               types.InlineKeyboardButton("Соц. супровід", callback_data="assistance_social"),
-               types.InlineKeyboardButton("Інше", callback_data="assistance_other"))
-    bot.send_message(chat_id, 'Яку домопогу ви потребуєте?', reply_markup=markup)
+    markup.add(types.InlineKeyboardButton("Food", callback_data="assistance_food"),
+               types.InlineKeyboardButton("Hygienic dialing", callback_data="assistance_hygiene_set"),
+               types.InlineKeyboardButton("Medicine", callback_data="assistance_meds"),
+               types.InlineKeyboardButton("Hygiene products (Diapers, pads, diapers)", callback_data="assistance_hygiene_items"),
+               types.InlineKeyboardButton("Soc. accompaniment", callback_data="assistance_social"),
+               types.InlineKeyboardButton("Other", callback_data="assistance_other"))
+    bot.send_message(chat_id, 'What help do you need?', reply_markup=markup)
 
 @bot.callback_query_handler(func=lambda call: call.data.startswith("assistance_"))
 def process_assistance(call):
@@ -168,15 +168,15 @@ def process_assistance(call):
         return
 
     if call.data == "assistance_other":
-        msg = bot.send_message(chat_id, 'Будь ласка вкажіть ваш варіант:')
+        msg = bot.send_message(chat_id, 'Please indicate your option:')
         bot.register_next_step_handler(msg, process_assistance_other)
     else:
         assistance_options = {
-            "assistance_food": "Харчування",
-            "assistance_hygiene_set": "Гігієнічний набір",
-            "assistance_meds": "Ліки",
-            "assistance_hygiene_items": "Засоби гігієни(Памперси, прокладки, пелюшки)",
-            "assistance_social": "Соц. Супровід"
+            "assistance_food": "Food",
+            "assistance_hygiene_set": "Hygienic dialing",
+            "assistance_meds": "Medicine",
+            "assistance_hygiene_items": "Hygiene products (Diapers, pads, diapers)",
+            "assistance_social": "Soc. accompaniment"
         }
         user_data[chat_id]['assistance'] = assistance_options[call.data]
         ask_consent(chat_id)
@@ -195,7 +195,7 @@ def ask_consent(chat_id):
     markup = types.InlineKeyboardMarkup()
     markup.add(types.InlineKeyboardButton("Так", callback_data="consent_yes"),
                types.InlineKeyboardButton("Ні", callback_data="consent_no"))
-    bot.send_message(chat_id, 'Чи згодні ви на обробку персональних даних? (Так/Ні)', reply_markup=markup)
+    bot.send_message(chat_id, 'Do you agree to the processing of personal data? (Yes/No)', reply_markup=markup)
 
 @bot.callback_query_handler(func=lambda call: call.data in ["consent_yes", "consent_no"])
 def process_consent(call):
@@ -204,13 +204,13 @@ def process_consent(call):
         start(call.message)
         return
 
-    user_data[chat_id]['consent'] = 'Так' if call.data == 'consent_yes' else 'Ні'
+    user_data[chat_id]['consent'] = 'Yes' if call.data == 'consent_yes' else 'No'
     
     if call.data == 'consent_no':
         markup = types.InlineKeyboardMarkup()
-        markup.add(types.InlineKeyboardButton("Так", callback_data="previous_assistance_yes"),
-                   types.InlineKeyboardButton("Ні", callback_data="previous_assistance_no"))
-        bot.send_message(chat_id, 'Чи отримували ви раніше допомогу від нашого фонду?', reply_markup=markup)
+        markup.add(types.InlineKeyboardButton("Yes", callback_data="previous_assistance_yes"),
+                   types.InlineKeyboardButton("No", callback_data="previous_assistance_no"))
+        bot.send_message(chat_id, 'Have you received help from our fund before?', reply_markup=markup)
     else:
         ask_how_heard(chat_id)
 
@@ -221,15 +221,15 @@ def process_previous_assistance(call):
         start(call.message)
         return
 
-    user_data[chat_id]['previous_assistance'] = 'Так' if call.data == 'previous_assistance_yes' else 'Ні'
+    user_data[chat_id]['previous_assistance'] = 'Yes' if call.data == 'previous_assistance_yes' else 'No'
     ask_how_heard(chat_id)
 
 def ask_how_heard(chat_id):
     markup = types.InlineKeyboardMarkup()
-    markup.add(types.InlineKeyboardButton("Соц. Мережі", callback_data="how_heard_social_media"),
-               types.InlineKeyboardButton("Знайомі", callback_data="how_heard_friends"),
-               types.InlineKeyboardButton("Інше", callback_data="how_heard_other"))
-    bot.send_message(chat_id, 'Звідки ви дізнались про нас?', reply_markup=markup)
+    markup.add(types.InlineKeyboardButton("Soc. Networks", callback_data="how_heard_social_media"),
+               types.InlineKeyboardButton("Acquaintances", callback_data="how_heard_friends"),
+               types.InlineKeyboardButton("Other", callback_data="how_heard_other"))
+    bot.send_message(chat_id, 'How did you hear about us?', reply_markup=markup)
 
 @bot.callback_query_handler(func=lambda call: call.data.startswith("how_heard_"))
 def process_how_heard(call):
@@ -239,12 +239,12 @@ def process_how_heard(call):
         return
 
     how_heard_options = {
-        "how_heard_social_media": "Соц. Мережі",
-        "how_heard_friends": "Знайомі",
-        "how_heard_other": "Інше"
+        "how_heard_social_media": "Soc. Networks",
+        "how_heard_friends": "Acquaintances",
+        "how_heard_other": "Other"
     }
     if call.data == "how_heard_other":
-        msg = bot.send_message(chat_id, 'Будь ласка вкажіть ваш варіант не більше 300 символів:')
+        msg = bot.send_message(chat_id, 'Please indicate your option in no more than 300 characters:')
         bot.register_next_step_handler(msg, process_how_heard_other)
     else:
         user_data[chat_id]['how_heard'] = how_heard_options[call.data]
@@ -261,7 +261,7 @@ def process_how_heard_other(message):
         user_data[chat_id]['how_heard'] = how_heard_other
         confirm_data(chat_id)
     else:
-        msg = bot.send_message(chat_id, 'Занадто великий текст введіть будь ласка не більше 300 символів:')
+        msg = bot.send_message(chat_id, 'The text is too long, please enter no more than 300 characters:')
         bot.register_next_step_handler(msg, process_how_heard_other)
 
 def confirm_data(chat_id):
@@ -270,21 +270,21 @@ def confirm_data(chat_id):
 
     user = user_data[chat_id]
     confirmation_message = (
-        f"Піб: {user['name']}\n"
-        f"Номер телефону: {user['phone']}\n"
-        f"ІНН: {user['inn']}\n"
-        f"Дата народження: {user['dob']}\n"
-        f"ВПО: {user['vpo']}\n"
-        f"Адреса проживання: {user['location']}\n"
-        f"Допомога: {user['assistance']}\n"
-        f"Вразлива категорія: {user.get('vulnerability', 'Не указано')}\n"
-        f"Згода на обробку данних: {user['consent']}\n"
-        f"Звідки ви дізнались про нас: {user['how_heard']}\n"
-        "Все вірно? (Так/Ні)"
+        f"Peeb: {user['name']}\n"
+        f"Phone number: {user['phone']}\n"
+        f"TIN: {user['inn']}\n"
+        f"Date of birth: {user['dob']}\n"
+        f"IDPs: {user['vpo']}\n"
+        f"Residential address: {user['location']}\n"
+        f"Help: {user['assistance']}\n"
+        f"Vulnerable category: {user.get('vulnerability', 'Не указано')}\n"
+        f"Consent to data processing:{user['consent']}\n"
+        f"How did you find out about us: {user['how_heard']}\n"
+        "Everything correct? (Yes/No)"
     )
     markup = types.InlineKeyboardMarkup()
-    markup.add(types.InlineKeyboardButton("Так", callback_data="confirm_yes"),
-               types.InlineKeyboardButton("Ні", callback_data="confirm_no"))
+    markup.add(types.InlineKeyboardButton("Yes", callback_data="confirm_yes"),
+               types.InlineKeyboardButton("No", callback_data="confirm_no"))
     bot.send_message(chat_id, confirmation_message, reply_markup=markup)
 
 @bot.callback_query_handler(func=lambda call: call.data in ["confirm_yes", "confirm_no"])
@@ -299,13 +299,13 @@ def process_confirmation(call):
         c.execute("INSERT INTO users (name, phone, inn, dob, vpo, location, assistance, vulnerability, consent, how_heard) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
                   (user['name'], user['phone'], user['inn'], user['dob'], user['vpo'], user['location'], user['assistance'], user.get('vulnerability', ''), user['consent'], user['how_heard']))
         conn.commit()
-        bot.send_message(chat_id, "Ваші дані збережено!")
+        bot.send_message(chat_id, "Your data is saved!")
     else:
-        bot.send_message(chat_id, "Введіть дані заново:")
+        bot.send_message(chat_id, "Enter the data again:")
         bot.register_next_step_handler(call.message, process_name)
 
 @bot.message_handler(func=lambda message: True)
 def echo_message(message):
-    bot.send_message(message.chat.id, "Будь ласка використовуйте /start для початку діалогу")
+    bot.send_message(message.chat.id, "Please use /start to start the dialog")
 
 bot.polling()
